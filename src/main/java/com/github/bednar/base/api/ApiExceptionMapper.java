337c3d849c238cad4d1c93381707f51ffca53fc6
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-11-11 19:31:56 +0100

    * Logging API exception

--- src/main/java/com/github/bednar/base/api/ApiExceptionMapper.java
+++ src/main/java/com/github/bednar/base/api/ApiExceptionMapper.java
@@ -4,15 +4,22 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Jakub Bednář (01/09/2013 8:59 AM)
  */
 @Provider
 public class ApiExceptionMapper implements ExceptionMapper<Exception>
 {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionMapper.class);
+
     @Override
     public Response toResponse(Exception exception)
     {
+        LOG.error("[mapping-exception]", exception);
+
         return Response
                 .status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(exception.getMessage())
